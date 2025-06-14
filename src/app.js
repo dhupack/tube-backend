ffd28edd50f0ecfc978b7f935959956a6b22c827
Deepak Kumar <dkprajapati20012004@gmail.com>
2025-06-14 21:46:41 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"})) // agr data as a ur
 app.use(express.static("public")) // agr jase kuch public asset ayege to uske liye h ye
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
 export { app }
