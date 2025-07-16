0e0881905b6e9458b03f8f7f6ab00cf0ce0b09d4
Deepak Kumar <dkprajapati20012004@gmail.com>
2025-07-17 02:47:35 +0530

    video comment removed

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,10 +8,8 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    // Step 1: Extract query parameters from request
+    
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-    // Step 2: Prepare match stage to filter videos
     const matchStage = {
         isPublished: true
     };
